f6323fe0f3e409d6797213cd5df35859d7e6bc69
Lynn Gong <lynn.gong@kellogg.northwestern.edu>
2017-04-21 11:34:01 -0500

    square new and results

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -13,4 +13,14 @@ class CalculationsController < ApplicationController
     render("calculations/flex_square_root.html.erb")
   end
 
+  def square_form
+    render("calculations/square_form.html.erb")
+  end
+
+  def square
+    #params =  {"number_to_be_squared"=>"42"}
+    @user_provided_number = params["number_to_be_squared"].to_f
+    @squared_number = @user_provided_number ** 2
+    render("calculations/square.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,15 @@ Rails.application.routes.draw do
       :controller => "calculations",
       :action => "flex_square_root"
     })
+
+    get("/square/new", {
+      :controller => "calculations",
+      :action => "square_form"
+    })
+
+    get("/square/results", {
+      :controller => "calculations",
+      :action => "square"
+    })
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
