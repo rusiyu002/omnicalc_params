7871dd4fcff72ffd05887b1733eea5632ee2f8e4
Lynn Gong <lynn.gong@kellogg.northwestern.edu>
2017-04-27 10:44:36 -0500

    homework finished

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -13,6 +13,23 @@ class CalculationsController < ApplicationController
     render("calculations/flex_square_root.html.erb")
   end
 
+  def flex_payment
+    # params = {"apr"=>"410", "year"=>"10", "total_amount"=>"20000"}
+    @apr = params["apr"].to_f.round(6)/10000
+    @year = params["year"].to_i
+    @principal = params["total_amount"].to_i
+    @monthly_payment = (@apr / 12 * @principal) / (1 - ((1 + @apr / 12 ) ** ((-1) * @year * 12)))
+    render("calculations/flex_payment.html.erb")
+  end
+
+  def flex_random
+    # Parameters: {"min"=>"1", "max"=>"20"}
+    @min = params["min"].to_i
+    @max = params["max"].to_i
+    @random = (@min + rand(@max-@min+1)).to_i
+    render("calculations/flex_random.html.erb")
+  end
+
   def square_form
     render("calculations/square_form.html.erb")
   end
@@ -23,4 +40,38 @@ class CalculationsController < ApplicationController
     @squared_number = @user_provided_number ** 2
     render("calculations/square.html.erb")
   end
+
+  def square_root_form
+    render("calculations/square_root_form.html.erb")
+  end
+
+  def square_root
+    @number = params["number_to_be_square_rooted"].to_f
+    @squared_rooted_number = Math.sqrt(@number)
+    render("calculations/square_root.html.erb")
+  end
+
+  def payment_form
+    render("calculations/payment_form.html.erb")
+  end
+
+  def payment
+    @apr = params["apr"].to_f.round(6)/100
+    @year = params["years"].to_i
+    @principal = params["principal"].to_i
+    @monthly_payment = (@apr / 12 * @principal) / (1 - ((1 + @apr / 12 ) ** ((-1) * @year * 12)))
+    render("calculations/payment.html.erb")
+  end
+
+  def random_form
+    render("calculations/random_form.html.erb")
+  end
+
+  def random
+      @min = params["min"].to_f
+      @max = params["max"].to_f
+      @random = (@min + rand(@max-@min+0.1)).to_f
+      render("calculations/random.html.erb")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,16 @@ Rails.application.routes.draw do
       :action => "flex_square_root"
     })
 
+    get("flexible/payment/:apr/:year/:total_amount", {
+      :controller => "calculations",
+      :action => "flex_payment"
+    })
+
+    get("flexible/random/:min/:max", {
+      :controller => "calculations",
+      :action => "flex_random"
+    })
+
     get("/square/new", {
       :controller => "calculations",
       :action => "square_form"
@@ -17,6 +27,36 @@ Rails.application.routes.draw do
     get("/square/results", {
       :controller => "calculations",
       :action => "square"
+    })
+
+    get("square_root/new", {
+      :controller => "calculations",
+      :action => "square_root_form"
+    })
+
+    get("square_root/results", {
+      :controller => "calculations",
+      :action => "square_root"
+    })
+
+    get("payment/new", {
+      :controller => "calculations",
+      :action => "payment_form"
+    })
+
+    get("payment/results", {
+      :controller => "calculations",
+      :action => "payment"
+    })
+
+    get("random/new", {
+      :controller => "calculations",
+      :action => "random_form"
+    })
+
+    get("random/results", {
+      :controller => "calculations",
+      :action => "random"
     })
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
